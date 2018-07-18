e07e1f5cc0caa8dd20b570d202f6d96bb05b2f76
mmm311 <1034930026@qq.com>
2018-07-18 18:58:14 +0800

    例子1

--- src/huawei/Problem01.java
+++ src/huawei/Problem01.java
@@ -1,4 +1,14 @@
 package huawei;
 
+import java.util.Scanner;
+
 public class Problem01 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a + b);
+        }
+    }
 }
